1507bad0547566492c127685663c913c5c191b0e
Ordinary56 <71382932+Ordinary56@users.noreply.github.com>
2023-02-14 16:23:56 +0100

    Exit is now fully working.

--- Main_Game/Program.cs
+++ Main_Game/Program.cs
@@ -16,10 +16,10 @@ namespace Menu
 
     class Program
     {
-        
-        
-        
-        public Dictionary<char,string[]> routeDict= new()
+
+
+
+        static public Dictionary<char, string[]> routeDict = new()
         {
             {'═',new string[]{"Nyugat","Kelet"}},
             {'╬', new string[]{"Észak","Dél","Nyugat","Kelet"}},
@@ -31,11 +31,10 @@ namespace Menu
             {'╗',new string[]{"Dél","Nyugat"}},
 
             {'╣', new string[]{"Észak","Dél","Nyugat"}},
-            {'╩',new string[]{ "Észak","Kelet","Nyugat"} },
             {'╚',new string[]{"Észak","Kelet"}},
 
         };
-        
+
 
 
         public delegate void CenterText();
@@ -43,16 +42,16 @@ namespace Menu
         public static ResourceManager rm = new ResourceManager("Main_Game.Resources.String", Assembly.GetExecutingAssembly());
 
 
-        static void HighlightSelected(List<Option> options, Option selected) 
+        static void HighlightSelected(List<Option> options, Option selected)
         {
-            foreach(Option option in options)
+            foreach (Option option in options)
             {
 
-                if(option == selected)
+                if (option == selected)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write("-> ");
-                    
+
                 }
                 else
                 {
@@ -66,11 +65,11 @@ namespace Menu
         static void Main(string[] args)
         {
             //This should work
-            if(Thread.CurrentThread.CurrentUICulture.Name != "hu-HU" && Thread.CurrentThread.CurrentUICulture.Name != "en-GB")
+            if (Thread.CurrentThread.CurrentUICulture.Name != "hu-HU" && Thread.CurrentThread.CurrentUICulture.Name != "en-GB")
             {
-                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB",true);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB", true);
             }
-  
+
 
             //Clear the Console Screen
             Console.Clear();
@@ -82,11 +81,10 @@ namespace Menu
         public static void MainMenu()
         {
             int index = 0;
-            
-            
+
+
             List<Option> options = new()
                 {
-
                     new Option(rm.GetString("Option1"),Language),
                     new Option( rm.GetString("Option2"),LoadMap),
                     new Option(rm.GetString("Option3"),() => Environment.Exit(0))
@@ -104,19 +102,19 @@ namespace Menu
                 switch (key.Key)
                 {
                     case ConsoleKey.W:
-                    case ConsoleKey.UpArrow:                      
+                    case ConsoleKey.UpArrow:
                         index = Math.Clamp(--index, 0, options.Count - 1);
                         break;
                     case ConsoleKey.S:
                     case ConsoleKey.DownArrow:
-                      index = Math.Clamp(++index,0,options.Count-1);
+                        index = Math.Clamp(++index, 0, options.Count - 1);
                         break;
                     case ConsoleKey.Enter:
                         options[index].Selected.Invoke();
                         break;
                 }
             }
-            
+
 
         }
 
@@ -142,12 +140,12 @@ namespace Menu
                 {
                     case ConsoleKey.W:
                     case ConsoleKey.UpArrow:
-                       index = Math.Clamp(--index, 0, langOpt.Count - 1);
+                        index = Math.Clamp(--index, 0, langOpt.Count - 1);
                         break;
                     case ConsoleKey.S:
                     case ConsoleKey.DownArrow:
-                        
-                       index = Math.Clamp(++index, 0, langOpt.Count - 1);
+
+                        index = Math.Clamp(++index, 0, langOpt.Count - 1);
                         break;
                     case ConsoleKey.Enter:
                         langOpt[index].Selected.Invoke();
@@ -163,30 +161,30 @@ namespace Menu
 
             //Generáljunk választható opciókat a beolvasott pályáknak megfelelően
             List<Option> maps = new();
-            
-            
+
+
             DirectoryInfo d = new(@"./Maps");
             FileInfo[] fullmaps = d.GetFiles("*.txt");
             FileInfo[] savedmaps = d.GetFiles("*.sav");
-            if(!(fullmaps.Count() > 0))
+            if (!(fullmaps.Count() > 0))
             {
                 Console.WriteLine(rm.GetString("findMapError"));
             }
             else
             {
                 //Index for the map List
-               int mapIndex = 0;
-               int Index = 1;
-               foreach(FileInfo f in fullmaps)
+                int mapIndex = 0;
+                int Index = 1;
+                foreach (FileInfo f in fullmaps)
                 {
 
-                    maps.Add(new Option($"[{Index}]. {f.Name}",() => FireGame(f.Name)));
+                    maps.Add(new Option($"[{Index}]. {f.Name}", () => FireGame(f.Name)));
                     Index++;
                 }
-               //Add exit for the last option
-                maps.Add(new Option($"[{maps.Count+1}].{rm.GetString("langESC").Substring(4)}", MainMenu));
+                //Add exit for the last option
+                maps.Add(new Option($"[{maps.Count + 1}].{rm.GetString("langESC").Substring(4)}", MainMenu));
                 HighlightSelected(maps, maps[mapIndex]);
-                
+
                 while (true)
                 {
                     Console.Clear();
@@ -204,7 +202,7 @@ namespace Menu
                             break;
                         case ConsoleKey.S:
                         case ConsoleKey.DownArrow:
-                             mapIndex = Math.Clamp(++mapIndex, 0, maps.Count - 1);
+                            mapIndex = Math.Clamp(++mapIndex, 0, maps.Count - 1);
                             break;
                         case ConsoleKey.Enter:
                             maps[mapIndex].Selected.Invoke();
@@ -214,13 +212,13 @@ namespace Menu
                 }
 
             }
-           
 
 
 
-           
 
-            
+
+
+
         }
         static void FireGame(string FileName)
         {
@@ -229,11 +227,11 @@ namespace Menu
             Maze maze = new Maze(FileName, diff);
             maze.CreateMap(ref FileName);
             maze.ShowMap();
-            Player player = new Player( maze);
+            Player player = new Player(maze);
             player.Move();
         }
 
-      
+
 
     }
 
@@ -242,7 +240,7 @@ namespace Menu
     ///<summary>Class for the Maze (Map). This object is Disposable</summary>
     public class Maze : IDisposable
     {
-        
+
         /*
             - Kilépés a labirintusból
         */
@@ -263,7 +261,7 @@ namespace Menu
         internal void CreateMap(ref string filename)
         {
             string[] file = File.ReadAllLines($".//Maps//{filename}");
-            if(file.Length == 0)
+            if (file.Length == 0)
             {
                 Console.WriteLine(Program.rm.GetString("findMapError"));
                 Console.ReadKey();
@@ -293,7 +291,7 @@ namespace Menu
                 Console.WriteLine();
             }
         }
-        public void Dispose() 
+        public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
@@ -305,7 +303,7 @@ namespace Menu
         protected virtual void Dispose(bool _disposing)
         {
             if (_disposed) return;
-            if(_disposing)
+            if (_disposing)
             {
                 this.map = null;
                 this.FileName = string.Empty;
@@ -319,7 +317,7 @@ namespace Menu
         /// Loops over the boundaries of the map and finds all the exits
         /// </summary>
         /// <returns>A list of all the entrance's coordinates</returns>
-        public List<(int,int)> GetRandomEntrance()
+        public List<(int, int)> GetRandomEntrance()
         {
             List<(int, int)> coords = new();
             for (int row = 0; row < map.GetLength(0); row++)
@@ -337,7 +335,7 @@ namespace Menu
                 }
             }
             return coords;
-            
+
 
         }
     }
@@ -353,93 +351,106 @@ namespace Menu
         delegate int Clamp(int val, int min, int max);
         Clamp clamp = Math.Clamp;
         private Maze Mz;
+
+        static int secs = 60;
+        tm.Timer timer = new tm.Timer(1000);
         const char CHARACTER_SPRITE = '░';
         private static int Counter;
         static int Steps;
-        static HashSet<(int,int)> stored = new();
-
+        static HashSet<(int, int)> stored = new();
         bool _disposed;
+        bool leftFromStart;
         public int posX { get; set; }
         public int posY { get; set; }
         public int NewPosX;
         public int NewPosY;
-     
+        (int, int) coords;
         private Random rnd = new();
 
         public Player(Maze mz)
         {
             this.Mz = mz;
         }
-       
+
         /// <summary>
         /// Main Game loop
         /// </summary>
+        
         public void Move()
-        { 
-            (int,int) coords = this.Mz.GetRandomEntrance()[rnd.Next(this.Mz.GetRandomEntrance().Count)];
+        {
+
+            coords = this.Mz.GetRandomEntrance()[rnd.Next(this.Mz.GetRandomEntrance().Count)];
             this.posX = coords.Item2;
-            this.posY = coords.Item1 +2;
+            this.posY = coords.Item1 + 2;
             this.NewPosX = this.posX;
             this.NewPosY = this.posY;
             Console.CursorVisible = false;
-            
+            timer.Start();
+            timer.Enabled = true;
+            timer.Elapsed += Timer_Elapsed;
+            Console.SetCursorPosition(this.posX, this.posY);
+            Console.Write(CHARACTER_SPRITE);
             while (true)
             {
-                Console.SetCursorPosition(0,0);
-                Console.Write($"Pálya neve: {this.Mz.FileName}, mérete: {this.Mz.Map.GetLength(0)} sor x {this.Mz.Map.GetLength(1)} oszlop\n Felfedezett termek száma: {this.RoomsFound()}\t");
-                Console.SetCursorPosition(this.posX, this.posY);
-                Console.Write(CHARACTER_SPRITE);
+
+
+
 
                 switch (Console.ReadKey(true).Key)
                 {
-                   
+
                     case ConsoleKey.W:
-                        this.NewPosY = clamp(--this.NewPosY, 0, this.Mz.Map.GetLength(0) + 1);
+                        this.NewPosY = clamp(--this.NewPosY, 2, this.Mz.Map.GetLength(0) + 1);
                         break;
                     case ConsoleKey.S:
-                        this.NewPosY = clamp(++this.NewPosY,0,this.Mz.Map.GetLength(0)+1);
+                        this.NewPosY = clamp(++this.NewPosY, 0, this.Mz.Map.GetLength(0) + 1);
                         break;
                     case ConsoleKey.A:
-                       this.NewPosX = clamp(--this.NewPosX,0,this.Mz.Map.GetLength(1)-1);
+                        this.NewPosX = clamp(--this.NewPosX, 0, this.Mz.Map.GetLength(1) - 1);
                         break;
                     case ConsoleKey.D:
-                        this.NewPosX = clamp(++this.NewPosX, 0, this.Mz.Map.GetLength(1) -1);
+                        this.NewPosX = clamp(++this.NewPosX, 0, this.Mz.Map.GetLength(1) - 1);
                         break;
                     case ConsoleKey.K:
                         this.SaveMap();
                         break;
-                    
+                   default:
+                        break;
+
                 }
-                Mz.ShowMap();
-                if (this.Mz.Map[clamp(NewPosY-2,0,this.Mz.Map.GetLength(0)-1),clamp(NewPosX,0,this.Mz.Map.GetLength(1)-1)] != '.')
+                if (this.Mz.Map[clamp(NewPosY - 2, 0, this.Mz.Map.GetLength(0) - 1), clamp(NewPosX, 0, this.Mz.Map.GetLength(1) - 1)] != '.')
                 {
+                    //Egyetlen mód arra, hogy Console.Clear nélkül mozogjunk, hogy az előző pozíciónknál kiírjuk a karaktert miután léptünk és UTÁNA
+                    //iratjuk ki a játékost
+                    Console.SetCursorPosition(this.posX, this.posY);
+                    Console.Write(this.Mz.Map[this.posY - 2, this.posX]);
                     this.posX = this.NewPosX;
                     this.posY = this.NewPosY;
+                    leftFromStart = true;
                 }
                 else
                 {
                     this.NewPosX = this.posX;
                     this.NewPosY = this.posY;
                 }
-               
+                Console.SetCursorPosition(this.posX, this.posY);
+                Console.Write(CHARACTER_SPRITE);
+                IsAtEntrace();
+                Exit();
                 Steps++;
-              
+
 
             }
         }
 
-        static int CountDown()
+        private void Timer_Elapsed(object? sender, tm.ElapsedEventArgs e)
         {
-            int seconds = 60;
-            while(seconds > 0)
-            {
-                Thread.Sleep(1000);
-                seconds--;
-            }
-            return seconds;
-            
-      
+            Console.SetCursorPosition(0, 0);
+            string CurrentRoute = string.Join(',',Program.routeDict[this.Mz.Map[this.posY-2,this.posX]]);
+            Console.Write($"Pálya neve: {this.Mz.FileName}, mérete: {this.Mz.Map.GetLength(0)} sor x {this.Mz.Map.GetLength(1)} oszlop\n Felfedezett termek száma: {this.RoomsFound()}\t{secs}\t {CurrentRoute.Replace(CurrentRoute,CurrentRoute)}");
+            secs--;
         }
+
 
         /// <summary>
         /// Keeps track of how many rooms does the player visited
@@ -447,29 +458,70 @@ namespace Menu
         /// <returns>the current number of rooms visited</returns>
         private int RoomsFound()
         {
-             
-            
-            if (this.Mz.Map[clamp(this.posY-2,0,this.Mz.Map.GetLength(0)-1), clamp(this.posX,0,this.Mz.Map.GetLength(1)-1)] == '█' && !(stored.Contains((this.posX,this.posY))))
+
+
+            if (this.Mz.Map[clamp(this.posY - 2, 0, this.Mz.Map.GetLength(0) - 1), clamp(this.posX, 0, this.Mz.Map.GetLength(1) - 1)] == '█' && !(stored.Contains((this.posX, this.posY))))
             {
-               Counter++;
-               stored.Add((this.posX, this.posY ));
+                Counter++;
+                stored.Add((this.posX, this.posY));
             }
             return Counter;
         }
 
-        private void SaveMap() 
+        private void SaveMap()
         {
-           
-           //idk...
-           //todo SAVE THIS GODDAMN THING
+
+            //idk...
+            //todo SAVE THIS GODDAMN THING
         }
-        
+
         void Exit()
         {
-            this.Mz.Dispose();
-            this.Dispose();
-            Program.MainMenu();
+            if(!IsAtEntrace())
+            {
+                return;
+            }
+            if(!(Counter < Method.GetRoomNumber(this.Mz.Map)))
+            {
+                Console.SetCursorPosition(0,Console.WindowHeight-3);
+                Console.Write(Program.rm.GetString("ExitSuccess"));
+                Thread.Sleep(3000);
+                this.Mz.Dispose();
+                this.Dispose();
+                Program.MainMenu();
+                
+            }
+            Console.SetCursorPosition(0, Console.WindowHeight - 3);
+            Console.Write(Program.rm.GetString("Exit"));
+            switch(Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.Y:
+                    this.Mz.Dispose();
+                    this.Dispose();
+                    Program.MainMenu();
+                    break;
+                    case ConsoleKey.N:
+                    Console.SetCursorPosition(0, Console.WindowHeight - 3);
+                    Console.Write(new string(' ',Console.WindowWidth));
+                    leftFromStart = false;
+                        return;
+                    default:
+                        return;
+
+                }
+
+            
         }
+        bool IsAtEntrace()
+        {
+           if(leftFromStart && this.Mz.GetRandomEntrance().Contains((this.posY-2,this.posX)))
+            {
+                return true;
+            }
+           return false;
+
+        }
+        
 
         public void Dispose()
         {
@@ -479,7 +531,7 @@ namespace Menu
         protected virtual void Dispose(bool _disposing)
         {
             if (_disposed) return;
-            if(_disposing)
+            if (_disposing)
             {
                 Counter = 0;
                 this.posX = default;
@@ -487,9 +539,10 @@ namespace Menu
                 this.Mz = null;
                 stored.Clear();
                 this.rnd = null;
-                
+                Steps = default;
+
             }
-            _disposed = true;    
+            _disposed = true;
         }
 
     }
